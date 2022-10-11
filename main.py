df320466db9a65005b791abccc43f70f7cf70907
Your Name <you@example.com>
2022-10-11 00:47:46 -0700

    code1

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ shortest="zooooooooooooooooooooooooooooooooooooo"
 longest="a"
 x=0
 while x<len(names1):
-    if ord(shortest[0])>ord(names1[x][0]) and len(shortest)>len(names1[x]):
-        shortest=names1[x]
-    if ord(longest[0])<ord(names1[x][0]) and len(longest)<len(names1[x]):
-        longest=names1[x]
+    if len(shortest)>=len(names1[x]):
+        shortest1=names1[x]
+    if ord(shortest[0])>ord(shortest1[0]):
+        shortest=shortest1
+    if len(longest)<len(names1[x]):
+        longest1=names1[x]
+    if ord(longest[0])<ord(longest1[0]):
+        longest=longest1
     x=x+1
 print(shortest, longest)
