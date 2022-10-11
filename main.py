06ad0c910287901ca72209d7cacc54e8f8df8a5e
Your Name <you@example.com>
2022-10-11 00:40:05 -0700

    code

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 # ******************************
 # Make your Code
-# ******************************
+# ****************************
+inputvalues=input("All names:")
+names1=inputvalues.split()
+shortest="zooooooooooooooooooooooooooooooooooooo"
+longest="a"
+x=0
+while x<len(names1):
+    if ord(shortest[0])>ord(names1[x][0]) and len(shortest)>len(names1[x]):
+        shortest=names1[x]
+    if ord(longest[0])<ord(names1[x][0]) and len(longest)<len(names1[x]):
+        longest=names1[x]
+    x=x+1
+print(shortest, longest)
